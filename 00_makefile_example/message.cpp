6b5dc94842e7340878e656756f7d5e844572bedf
Zimrahin <diegobadillosj@gmail.com>
2025-02-15 19:25:48 +0100

    basic Hello World! example with Makefile

---
+++ 00_makefile_example/message.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "message.h"
+
+using namespace std;  // Not used
+
+void message::printMessage() {
+    cout << "Hello world!\n";
+}
---
+++ 00_makefile_example/message.h
@@ -0,0 +1,10 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+class message {
+  public:
+    // Function declarations
+    void printMessage();
+};
+
+#endif  // MESSAGE_H
