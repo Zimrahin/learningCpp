d59aee1b845f3f4b33e1b42e8342f2188020109d
Zimrahin <diegobadillosj@gmail.com>
2025-03-23 23:33:06 +0100

    space invaders: add OpenGL context

--- 03_space_invaders/main.cpp
+++ 03_space_invaders/main.cpp
@@ -14,6 +14,12 @@ int main() {
     if (!glfwInit())
         return -1;
 
+    // OpenGL version at least 4.0. Core profile: explude deprecated features
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     GLFWwindow *window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
     if (!window) {
         glfwTerminate();
